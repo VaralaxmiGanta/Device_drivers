da726ea23cb4eaced25f9b5970e6a6d440467ee7
srijyothsna18 <159214576+srijyothsna18@users.noreply.github.com>
2024-12-21 11:50:56 +0530

    Delete I2C directory

--- I2C/test_I2C27_sensor_supp.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-def test_lm75_adapter_name():
-    # Run sensors command and check for the adapter name
-    sensors_output = subprocess.check_output("sensors", shell=True).decode('utf-8')
-    print(sensors_output)
-    print("\nSensors are successfully listed by lm-sensnors package")
-
-
